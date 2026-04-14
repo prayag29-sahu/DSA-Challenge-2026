69facdb3f990f53ecc7b9f0c659446149562f0f2
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-14 21:15:44 +0530

    two sum problem solve in cpp

--- 01_Two_Sum.cpp
+++ 01_Two_Sum.cpp
@@ -9,10 +9,10 @@ vector<int> Two_Sum(vector<int>& nums, int target){
     int n = nums.size();
     for (int i = 0; i < n; i++){
         int complement = target - nums[i];
-        if(val.count(complement)){
+        if (val.find(complement) != val.end()){ // val.find(complement) find krega ki map me complement value ha ki nhi agr ha to vo val.end() ke sath match nhi hoga and true return krega else false , the alternate approch we can use val.cou t(complement) this directly return true and false but not provide value which was not needed here 
             return {val[complement], i};
         }
-        val[nums[i]] = i;
+        val[nums[i]] = i; // here we add the val and index on map val, in this method if the value already exist it override/update the value and index, we also have an alternate method val.insert({nums[i], i}); in this case if the value already have it skip the insertion so its not flexible.  
     }
     return {};
 }
