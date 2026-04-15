8ccd47c055ebd418afcc408b80f2d8e27f2ae2fb
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-15 23:59:26 +0530

    maximum subarray problem in cpp print array

--- 06_Maximum_subarray.cpp
+++ 06_Maximum_subarray.cpp
@@ -3,8 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-
-
 // My 1st attempt
 // int maxSubArray(vector<int> &nums)
 // {
@@ -21,20 +19,50 @@ using namespace std;
 //     return sum;
 // }
 
+// Optimized approach
+// int maxSubArray(vector<int> &nums){
+//     int currentSum = nums[0];
+//     int maxSum = nums[0];
+//     for (int i = 1; i < (int)nums.size(); i++){
+//         currentSum = max(nums[i], currentSum + nums[i]);
+//         maxSum = max(currentSum, maxSum);
+//     }
+//     return maxSum;
+// }
 
-
-// Optimized approach 
-int maxSubArray(vector<int> &nums){
+// Print max sub array
+int maxSubArray(vector<int> &nums)
+{
     int currentSum = nums[0];
     int maxSum = nums[0];
-    for (int i = 1; i < (int)nums.size(); i++){
-        currentSum = max(nums[i], currentSum + nums[i]);
-        maxSum = max(currentSum, maxSum);
+    int start = 0, end = 0, index = 0;
+    for (int i = 1; i < (int)nums.size(); i++)
+    {
+        // currentSum = max(nums[i], currentSum + nums[i]);
+        if (nums[i] > currentSum + nums[i])
+        {
+            currentSum = nums[i];
+            index = i;
+        }
+        else
+        {
+            currentSum += nums[i];
+        }
+
+        // maxSum = max(currentSum, maxSum);
+        if (currentSum > maxSum)
+        {
+            currentSum = maxSum;
+            start = index;
+            end = i;
+        }
     }
+
     return maxSum;
 }
 
-int main(){
+int main()
+{
     vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     cout << maxSubArray(nums);
 }
