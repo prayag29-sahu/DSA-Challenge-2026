451d06261d3d20a8894bf970193994ef28940d34
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-21 12:37:52 +0530

    find minimum in rotated sorted array using binary serach with rotated sorted

--- 12_find_minimum_in_rotated_sorted_array.cpp
+++ 12_find_minimum_in_rotated_sorted_array.cpp
@@ -3,30 +3,47 @@
 #include <vector>
 using namespace std;
 
-// 1st attempt
-int findMin(vector<int> &nums)
-{
-    int size = nums.size();
+// 1st attempt using 2 pointer
+// int findMin(vector<int> &nums)
+// {
+//     int size = nums.size();
+//     int left = 0;
+//     int right = size - 1;
+//     int mi = 20000;
+//     int m = 0;
+//     while (left < right)
+//     {
+//         if (nums[left] > nums[right])
+//         {
+//             m = nums[right];
+//             mi = min(m, mi);
+//         }
+//         else
+//         {
+//             m = nums[left];
+//             mi = min(m, mi);
+//         }
+//         ++left;
+//         --right;
+//     }
+//     return mi;
+// }
+
+// Optimized using binary search
+int findMin(vector<int> &nums){
     int left = 0;
-    int right = size - 1;
-    int mi = 20000;
-    int m = 0;
-    while (left < right)
-    {
-        if (nums[left] > nums[right])
-        {
-            m = nums[right];
-            mi = min(m, mi);
+    int right = nums.size() - 1;
+    while(left<right){
+        if(nums[left]<nums[right])
+            return nums[left];
+        int mid = left + (right - left) / 2;
+        if(nums[mid]>nums[right]) {
+            left = mid + 1;
+        } else{
+            right = mid;
         }
-        else
-        {
-            m = nums[left];
-            mi = min(m, mi);
-        }
-        ++left;
-        --right;
     }
-    return mi;
+    return nums[left];
 }
 
 int main()
