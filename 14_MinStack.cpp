16602aab15a8efeb867071e249603319a44ed3c7
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-22 22:25:01 +0530

    minstack problem using 2 stack

--- 14_MinStack.cpp
+++ 14_MinStack.cpp
@@ -4,35 +4,76 @@
 
 using namespace std;
 
-// my 1st approach
-class MinStack {
+// // my 1st approach
+// class MinStack {
+// private:
+//     stack<int> st;
+// public:
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         st.push(val);
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//        return st.top();
+//     }
+    
+//     int getMin() {
+//         stack<int> temp = st;
+//         int min_val = INT_MAX;
+//         while(!temp.empty()){
+//             min_val = min(min_val, temp.top());
+//             temp.pop();
+//         }
+//         return min_val;
+//     }
+// };
+
+// 2 stack
+class MinStack
+{
 private:
     stack<int> st;
+
+    stack<int> minst;
+
 public:
-    MinStack() {
-        
+    MinStack()
+    {
     }
-    
-    void push(int val) {
+
+    void push(int val)
+    {
         st.push(val);
+
+        if(minst.empty()|| val<minst.top()){
+            minst.push(val);
+        }
     }
-    
-    void pop() {
+
+    void pop()
+    {
+        if(st.top()==minst.top()){
+            minst.pop();
+        }
         st.pop();
     }
-    
-    int top() {
-       return st.top();
+
+    int top()
+    {
+        return st.top();
     }
-    
-    int getMin() {
-        stack<int> temp = st;
-        int min_val = INT_MAX;
-        while(!temp.empty()){
-            min_val = min(min_val, temp.top());
-            temp.pop();
-        }
-        return min_val;
+
+    int getMin()
+    {
+        return minst.top();
     }
 };
 
