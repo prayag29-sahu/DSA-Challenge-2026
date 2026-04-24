a3249289e7df0896a766544b36e44b1babac63c1
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-25 00:05:01 +0530

    invert binary tree print

--- 15_Invert_Binary_Tree.cpp
+++ 15_Invert_Binary_Tree.cpp
@@ -1,29 +1,78 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+#include <iostream>
+#include <queue>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
 public:
-    TreeNode* invertTree(TreeNode* root) {
-         if (!root) return nullptr;
-          queue<TreeNode*> q;  
-        q.push(root);        
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (!root)
+            return nullptr;
+        queue<TreeNode *> q;
+        q.push(root);
 
-        while (!q.empty()) {
-              TreeNode* node = q.front(); 
-            q.pop();                      
+        while (!q.empty())
+        {
+            TreeNode *node = q.front();
+            q.pop();
             swap(node->left, node->right);
-            if (node->left)  q.push(node->left);   
-            if (node->right) q.push(node->right); t
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
         }
-
-        return root;  
+        return root;
     }
 };
+
+void printLevelOrder(TreeNode *root)
+{
+    if (!root)
+        return;
+
+    queue<TreeNode *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        TreeNode *node = q.front();
+        q.pop();
+        cout << node->val << " ";
+
+        if (node->left)
+            q.push(node->left);
+        if (node->right)
+            q.push(node->right);
+    }
+    cout << endl;
+}
+
+int main(){
+    TreeNode *root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    Solution obj;
+
+    cout << "Original Tree: ";
+    printLevelOrder(root);
+
+    root = obj.invertTree(root);
+
+    cout << "Inverted Tree: ";
+    printLevelOrder(root);
+
+    return 0;
+}
