f62ad55750b44f633ebbd0529f16d2f0fb5117df
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-27 23:01:17 +0530

    about array declaration

---
+++ cpp_Basics/02_Array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int arr1[10]; // declaration of array datatype name[size]
+    
+    return 0;
+}
