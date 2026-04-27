547b195f3157fbb3bc7ac4280cf29bd418ae56dc
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-27 23:26:15 +0530

    modify stringcd 'f:\Documents\PrayagSahu\DSA_2026\cpp_Basics\output'

--- cpp_Basics/03_String.cpp
+++ cpp_Basics/03_String.cpp
@@ -21,6 +21,18 @@ public:
             cout << s[i] << "\t";
         }
     }
+    // Function to modify the string
+    string modifyString(string str)
+    {
+        // Assign str to a new variable
+        string newStr = str;
+
+        // Modify the new string
+        newStr[0] = 'H';
+
+        // Return the modified string
+        return newStr;
+    }
 };
 
 // Driver code
@@ -33,5 +45,13 @@ int main()
     // Call function and print result
     cout << obj.findLength(s) << endl;
     obj.accessCharacters(s);
+
+    // Call modifyString and store the result
+    string modified = obj.modifyString(s);
+
+    // Print both strings
+    cout << "Original String: " << s << endl;
+    cout << "Modified String: " << modified << endl;
+
     return 0;
 }
