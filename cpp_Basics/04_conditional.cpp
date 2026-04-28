163af466629c701583708b4f2e9c85354afb998d
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-29 00:10:53 +0530

    add student grade function using if else

--- cpp_Basics/04_conditional.cpp
+++ cpp_Basics/04_conditional.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
 using namespace std;
 
+class Solution
+{
+public:
+    void studentGrade(int marks)
+    {
+        if (marks < 35)
+        {
+            cout << "Fail";
+        }
+        else if (marks < 50)
+        {
+            cout << "Grade D";
+        }
+        else if (marks < 70)
+        {
+            cout << "Grade C";
+        }
+        else if (marks < 90)
+        {
+            cout << "Grade B";
+        }
+        else
+        {
+            cout << "Grade A";
+        }
+    }
+};
+
 int main()
 {
     int x = 0;
     int y = 4;
-    if(y)
-        cout << y;
-    if(x)
-        return 0;
+
+    Solution obj;
+
+    obj.studentGrade(55);
+
+    // if(y)
+    //     cout << y;
+    // if(x)
+    //     return 0;
     return 0;
 }
