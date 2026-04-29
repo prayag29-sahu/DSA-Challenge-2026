30a822dc46e30108ba031e5ab4c8a7a42aa05334
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-29 23:55:36 +0530

    add swich case condition function

--- cpp_Basics/04_conditional.cpp
+++ cpp_Basics/04_conditional.cpp
@@ -4,43 +4,76 @@ using namespace std;
 class Solution
 {
 public:
-    void studentGrade(int marks)
+    // if else case
+    char studentGrade(int marks)
     {
         if (marks < 35)
         {
             cout << "Fail";
+            return 'F';
         }
         else if (marks < 50)
         {
             cout << "Grade D";
+            return 'D';
         }
         else if (marks < 70)
         {
             cout << "Grade C";
+            return 'C';
         }
         else if (marks < 90)
         {
             cout << "Grade B";
+            return 'B';
         }
         else
         {
             cout << "Grade A";
+            return 'A';
+        }
+    }
+
+    // switch case
+    void SwitchStudentGrade(char Grade)
+    {
+        switch (Grade)
+        {
+        case 'A':
+            cout << "above the 90 marks";
+            break;
+        case 'B':
+            cout << "above the 70 marks";
+            break;
+        case 'C':
+            cout << "above the 50 marks";
+            break;
+        case 'D':
+            cout << "above the 30 marks";
+            break;
+        case 'E':
+            cout << "Fail";
+            break;
         }
     }
 };
 
 int main()
 {
-    int x = 0;
-    int y = 4;
-
-    Solution obj;
-
-    obj.studentGrade(55);
-
+    // int x = 0;
+    // int y = 4;
     // if(y)
     //     cout << y;
     // if(x)
     //     return 0;
+
+    int marks;
+    cout << "Enter the marks : ";
+    cin >> marks;
+    Solution obj;
+    char result = obj.studentGrade(marks);
+    cout << "\n";
+    obj.SwitchStudentGrade(result);
+
     return 0;
 }
