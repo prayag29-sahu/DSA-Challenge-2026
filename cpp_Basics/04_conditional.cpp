0928a7ccc542b1b1508974aaa484a66a260d4d61
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-30 00:19:04 +0530

    add swich case condition function

--- cpp_Basics/04_conditional.cpp
+++ cpp_Basics/04_conditional.cpp
@@ -54,8 +54,24 @@ public:
         case 'F':
             cout << "Fail";
             break;
-        default :
+        default:
             cout << "enter vaid marks. ";
+        }
+    }
+
+    void SwitchEvaluation(int x, int y)
+    {
+        // Evaluate the sum of x and y using a switch statement
+        switch (x + y)
+        {
+        case 15: // If the sum equals 15
+            cout << "Result is 15." << endl;
+            break; // Exit the switch block
+        case 20:   // If the sum equals 20
+            cout << "Result is 20." << endl;
+            break; // Exit the switch block
+        default:   // If no case matches
+            cout << "No match found." << endl;
         }
     }
 };
