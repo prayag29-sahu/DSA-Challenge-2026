064406771d895e697c761d73969fd3be0402907e
Prayag Sahu <sahuprayag229@gmail.com>
2026-04-30 23:55:12 +0530

    add loops file start with do while loop

--- cpp_Basics/05_Loops.cpp
+++ cpp_Basics/05_Loops.cpp
@@ -5,13 +5,20 @@ int main(){
 
     // for loop
     for (int i = 0; i < 5; ++i){
-        cout << i << "\n";
+        cout << i << "\t";
     }
+    cout << endl;
 
     // while loop
-    int i;
-    while(i<10){
+    int i=0;
+    // while(i<10){
+    //     cout << i << "\t";
+    //     ++i;
+    // }
+
+    // do while loop
+    do{
         cout << i << "\t";
         ++i;
-    }
+    } while (i < 5);
 }
