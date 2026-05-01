7d4c4e1050668868af27a1fabca9b5e3f83983c6
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-01 22:08:04 +0530

    implement pass by reference function

--- cpp_Basics/06_Functions.cpp
+++ cpp_Basics/06_Functions.cpp
@@ -6,12 +6,21 @@ void passByValue(int a){
     a += 5;
 }
 
+// Pass by reference
+void passByReference(int &a){
+    a += 5;
+}
 
 int main(){
     int x = 5;
     // pass by value
     passByValue(x);
     cout << x<<"\n";
+
+    // Pass by reference
+    passByReference(x);
+    cout << x<<"\n";
+
 
 
 }
