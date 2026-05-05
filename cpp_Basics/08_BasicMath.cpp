92e07a67f3431cc18b913f7da108c647f480591d
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-05 20:41:54 +0530

    add armstrong check function

--- cpp_Basics/08_BasicMath.cpp
+++ cpp_Basics/08_BasicMath.cpp
@@ -81,18 +81,43 @@ public:
         }
         return n1;
     }
+
+    // Armstrong number 153 = 1^3+5^3+3^3 = 153 ?
+    bool isArmstrong(int a)
+    {
+        int n = a;
+        int p = (int)(log10(a) + 1);
+        int arm = 0;
+        while (n > 0)
+        {
+            int rem = n % 10;
+            int pow = 1;
+            for (int i = 0; i < p; i++)
+            {
+                pow *= rem;
+            }
+            arm += pow;
+            n /= 10;
+        }
+        if (arm == a)
+        {
+            return true;
+        }
+        return false;
+    }
 };
 
 int main()
 {
     mathematics m;
-    int num = 123;
+    int num = 153;
     int n1 = 15, n2 = 18;
     // count digits in number
     cout << "Total number of digits in " << num << " is : " << m.countDigits(num) << "\n";
     cout << "The reverse of Number " << num << " is : " << m.reverseNumber(num) << "\n";
-    cout << "Check the palindrom for " << num << " is : " << m.isPalindrome(12321) << "\n";
+    cout << "Check the palindrom for " << num << " is : " << boolalpha << m.isPalindrome(12321) << "\n";
     cout << "GCD of  " << n1 << " and " << n2 << " is : " << m.GCD(n1, n2) << "\n";
     // lcm of a,b  = a*b/gcd of a,b
     cout << "LCM of  " << n1 << " and " << n2 << " is : " << (n1 * n2) / m.GCD(n1, n2) << "\n";
+    cout << "Check number : " << num << " is Armstrong : " << boolalpha << m.isArmstrong(num) << "\n";
 }
