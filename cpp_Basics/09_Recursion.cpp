2b25a3ca7737bec4773d625866eaad027aa65a29
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-06 20:55:10 +0530

    add recursive function to print 1 to n digits

--- cpp_Basics/09_Recursion.cpp
+++ cpp_Basics/09_Recursion.cpp
@@ -4,24 +4,38 @@ using namespace std;
 class Recursion
 {
 public:
-    int namePrint(int n, string name)
+    // print n time name
+    int name_Print(int n, string name)
     {
         if (n == 0)
             return 0;
         cout << name << " ";
-        namePrint(n - 1, name);
+        name_Print(n - 1, name);
+    }
+
+    // print 1 to n using recursion
+    int print_N_Numbers(int i,int n){
+        if(i>n)
+            return 0;
+        cout << i << " ";
+        print_N_Numbers(++i,n);
     }
 };
 int main()
 {
     // print n time name
     Recursion r;
-    int n;
-    string name;
-    cout << "Enter a number to print name : ";
-    cin >> n;
-    cout << "Enter a name to print : ";
-    cin >> name;
-    // r.namePrint(n, name);
-    r.namePrint(5, "Prayag");
+    // int n;
+    // string name;
+    // cout << "Enter a number to print name : ";
+    // cin >> n;
+    // cout << "Enter a name to print : ";
+    // cin >> name;
+    // r.name_Print(n, name);
+    cout << "Print n time name : ";
+    r.name_Print(5, "Prayag");
+
+    // print 1 to n using recursion
+    cout << "\nPrint 1 to n digits : ";
+    r.print_N_Numbers(1,5);
 }
