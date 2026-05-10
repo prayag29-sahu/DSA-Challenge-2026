fe15a3244a0a336b19faca26f87abb986c333a0a
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-10 23:10:30 +0530

    add Products of array except self using optimized prefix and suffix method

--- neetcode/07_Products_of_array_except_self.cpp
+++ neetcode/07_Products_of_array_except_self.cpp
@@ -21,6 +21,26 @@ public:
         }
         return results;
     }
+
+    // Optimized approach O(n) using prefix and prefix
+    vector<int> ProductOfArrayExceptItself01(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> results(n, 1);
+        int prefix = 1;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            results[i] = prefix;
+            prefix *= nums[i];
+        }
+        int suffix = 1;
+        for (int i = nums.size()-1; i >= 0; --i)
+        {
+            results[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return results;
+    }
 };
 
 int main()
@@ -28,9 +48,10 @@ int main()
     Products_of_array_except_self P;
     vector<int> nums = {1, 2, 4, 6};
     vector<int> answer;
-    answer = P.ProductOfArrayExceptItself(nums);
+    // answer = P.ProductOfArrayExceptItself(nums);
+    answer = P.ProductOfArrayExceptItself01(nums);
     for (int i = 0; i < answer.size(); ++i)
         cout << answer[i] << " ";
 
-    return;
+    return 0;
 }
