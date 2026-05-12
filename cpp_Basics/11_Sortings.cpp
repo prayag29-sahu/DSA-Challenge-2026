7f1fa6865e160a2a98c720dbd5fba805d1569dd0
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-12 12:48:22 +0530

    add insertion sort logic

--- cpp_Basics/11_Sortings.cpp
+++ cpp_Basics/11_Sortings.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class sorting
+class Sorting
 {
 public:
     void selection_sort(int num[], int n)
@@ -11,7 +11,7 @@ public:
             int min = i;
             for (int j = i + 1; j < n; ++j)
             {
-                if (num[j] <= num[min])
+                if (num[j] < num[min])
                     min = j;
             }
             int temp = num[i];
@@ -23,7 +23,7 @@ public:
     {
         for (int i = 0; i < n - 1; ++i)
         {
-            for (int j = 0; j < n - i; ++j)
+            for (int j = 0; j < n - i - 1; ++j)
             {
                 if (num[j] > num[j + 1])
                 {
@@ -32,15 +32,30 @@ public:
             }
         }
     }
+// insertion sort
+    void insertion_sort(int arr[], int n){
+        for (int i = 1; i < n ;++i){
+            int key = arr[i];
+            int j = i - 1;
+            while(j>=0 and arr[j]>key){
+                arr[j + 1] = arr[j];
+                --j;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+
 };
 
 int main()
 {
-    sorting s;
+    Sorting s;
     int arr[] = {5, 2, 4, 1, 7, 5, 10, 3, 43, 12, 65, 0, 34, -1};
     int n = 14;
     // s.selection_sort(arr, n);
-    s.bubble_sort(arr, n);
+    // s.bubble_sort(arr, n);
+    s.insertion_sort(arr, n);
     for (int i = 0; i < n; ++i)
     {
         cout << arr[i] << " ";
