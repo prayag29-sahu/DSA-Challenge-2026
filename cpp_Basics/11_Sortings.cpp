ead90791a656b51b69d91fbaf792d09d45a1a055
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-15 23:59:05 +0530

    add merge sort

--- cpp_Basics/11_Sortings.cpp
+++ cpp_Basics/11_Sortings.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Sorting
@@ -32,12 +33,15 @@ public:
             }
         }
     }
-// insertion sort
-    void insertion_sort(int arr[], int n){
-        for (int i = 1; i < n ;++i){
+    // insertion sort
+    void insertion_sort(int arr[], int n)
+    {
+        for (int i = 1; i < n; ++i)
+        {
             int key = arr[i];
             int j = i - 1;
-            while(j>=0 and arr[j]>key){
+            while (j >= 0 and arr[j] > key)
+            {
                 arr[j + 1] = arr[j];
                 --j;
             }
@@ -45,7 +49,57 @@ public:
         }
     }
 
+    // conquer method
+    void conquer(vector<int> &arr, int low, int mid, int high)
+    {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
+        while (left <= mid && right <= high)
+        {
+            if (arr[left] <= arr[right])
+            {
+                temp.push_back(arr[left]);
+                ++left;
+            }
+            else
+            {
+                temp.push_back(arr[right]);
+                ++right;
+            }
+        }
+        while (left <= mid)
+        {
+            temp.push_back(arr[left]);
+            ++left;
+        }
+        while (right <= high)
+        {
+            temp.push_back(arr[right]);
+            ++right;
+        }
+        for (int i = low; i <= high; ++i)
+        {
+            arr[i] = temp[i - low];
+        }
+    }
 
+    // Divide method
+    void divide(vector<int> &arr, int low, int high)
+    {
+        if (low >= high)
+            return;
+        int mid = low + (high - low) / 2;
+        divide(arr, low, mid);
+        divide(arr, mid + 1, high);
+        conquer(arr, low, mid, high);
+    }
+    // merge sort
+    vector<int> merge_sort(vector<int> &arr, int n)
+    {
+        divide(arr, 0, n);
+        return arr;
+    }
 };
 
 int main()
@@ -55,9 +109,13 @@ int main()
     int n = 14;
     // s.selection_sort(arr, n);
     // s.bubble_sort(arr, n);
-    s.insertion_sort(arr, n);
-    for (int i = 0; i < n; ++i)
-    {
-        cout << arr[i] << " ";
-    }
+    // s.insertion_sort(arr, n);
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+    vector<int> nums = {5, 2, 4, 1, 7, 5, 10, 3, 43, 12, 65, 0, 34, -1};
+    s.merge_sort(nums, nums.size() - 1);
+    for (int x : nums)
+        cout << x << " ";
 }
