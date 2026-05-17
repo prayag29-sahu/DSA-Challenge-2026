13c9da28ef7d2d5cecece1c7289aacf74676833d
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-17 23:27:02 +0530

    add longest consecutive function

---
+++ neetcode/08_Longest_Consecutive.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int LongestConsecutive(vector<int> &nums)
+{
+    if (nums.empty())
+        return 0;
+    unordered_set<int> set(nums.begin(), nums.end());
+    int length = 0;
+    for(int num :set){
+        if(set.find(num-1)==set.end()){
+            int currVal = num;
+            int currLenght = 1;
+            while(set.find(currVal+1)!=set.end()){
+                currVal++;
+                currLenght++;
+            }
+            length = max(length, currLenght);
+        }
+    }
+    return length;
+}
+
+int main()
+{
+    vector<int> nums = {2, 20, 4, 10, 3, 4, 5};
+    cout <<"Longest Consecutive is : " << LongestConsecutive(nums) << endl;
+}
