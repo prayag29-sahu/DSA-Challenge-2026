c67861ca2f48e2b1a57f3d9086b272cea49d2e37
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-20 21:27:07 +0530

    print first and second largest element of array

--- cpp_Basics/12_Array_easy.cpp
+++ cpp_Basics/12_Array_easy.cpp
@@ -8,7 +8,7 @@ public:
     int largestElement(int num[], int size)
     {
         int max = num[0];
-        for (int i = 0; i < size; ++i)
+        for (int i = 1; i < size; ++i)
         {
             if (max < num[i])
             {
@@ -38,6 +38,22 @@ public:
         }
         return maximum;
     }
+
+    // find second largest element 2,4,1,6,3,7,4
+    void secondLargestElement(int num[], int n)
+    {
+        int first = num[0]; // 2
+        int second;         // . ,
+        for (int i = 1; i < n; ++i)
+        {
+            if (first < num[i])
+            {                   // 2<4 t, 4<1 f, 4<6 t, 6<3 f, 6<7 t, 7<4 f
+                second = first; // 2 , 4 , 6
+                first = num[i]; // 4 , 6 , 7
+            }
+        }
+        cout << "First Largest Element of array : " << first << " and Second Largest Element of array : " << second;
+    }
 };
 
 int main()
@@ -46,5 +62,6 @@ int main()
     int arr[] = {12, 2, 33, 2, 7, 31, 8, 48, 9};
     int size = sizeof(arr) / sizeof(arr[0]);
     cout << "Largest Element of array using iteration : " << a.largestElement(arr, size) << endl;
-    cout << "Largest Element of array using 2 pointer : " << a.largestElement2Pointer(arr, size);
+    cout << "Largest Element of array using 2 pointer : " << a.largestElement2Pointer(arr, size) << "\n";
+    a.secondLargestElement(arr, size);
 }
