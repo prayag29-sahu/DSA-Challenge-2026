65dcc850b41ea53d0a16dc8bc1268f91bdf57fc7
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-28 00:07:03 +0530

    add a function to calculate the next warmest day distance using brute force

---
+++ neetcode/17_Daily_Temperatures.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+// Brute force approach
+vector<int> daily_temperature(vector<int> &temperatures)
+{
+    int n = temperatures.size();
+    vector<int> res(n, 0);
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (temperatures[j] > temperatures[i])
+            {
+                res[i] = j - i;
+                break;
+            }
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    vector<int> temperature = {30, 38, 30, 36, 35, 40, 28};
+    vector<int> results = daily_temperature(temperature);
+    for (int i : results)
+    {
+        cout << i << " ";
+    }
+}
