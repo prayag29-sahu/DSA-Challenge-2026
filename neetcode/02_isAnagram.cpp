55288793e828dafdda2092d272e05296af890a2c
Prayag Sahu <sahuprayag229@gmail.com>
2026-06-06 16:31:55 +0530

    revison add alternate method unordered map to check anagram

--- neetcode/02_isAnagram.cpp
+++ neetcode/02_isAnagram.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class IsAnagram
 {
 public:
+    // used when talk about only small alphabets
     bool isAnagram(string s, string t)
     {
         if (s.size() != t.size())
@@ -24,12 +25,45 @@ public:
 
         return true;
     }
+
+    // used when mixed small , capital, alphanumeric, symobls
+    bool isUniverseAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        unordered_map<char, int> str;
+        for (char c : s)
+        {
+            str[c]++;
+        }
+        for (char c : t)
+        {
+            str[c]--;
+        }
+        for (auto it : str)
+        {
+            if(it.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
 };
 
 int main()
 {
     IsAnagram a;
-    bool ans = a.isAnagram("abcde", "decba");
-    cout << boolalpha;
-    cout << ans;
+    // bool ans = a.isAnagram("abcde", "decba");
+    bool ans = a.isUniverseAnagram("abcde", "decba");
+    cout << boolalpha << ans;
 }
+
+// TC: O(n)
+// SC: O(1)
+// We count character frequencies from the first string
+// and subtract frequencies using the second string.
+// Since the frequency array size is fixed (26 lowercase letters),
+// extra space remains constant.
