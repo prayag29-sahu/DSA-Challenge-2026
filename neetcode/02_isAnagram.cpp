549912e9ead70143d163c54ff6eb5779266a29be
Prayag Sahu <sahuprayag229@gmail.com>
2026-06-06 16:41:52 +0530

    revison add alternate method sorting to check anagram

--- neetcode/02_isAnagram.cpp
+++ neetcode/02_isAnagram.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class IsAnagram
 {
 public:
     // used when talk about only small alphabets
+    // TC O(n) : O(n)+O(n)+O(n) for 3 loop total O(n)
+    // SC O(1) : O(26) for constant elements 
     bool isAnagram(string s, string t)
     {
         if (s.size() != t.size())
             return false;
-        vector<int> v(26, 0);
+        vector<int> v(26, 0); // allocate 26 space and assign 0 in all form 0 to 25
+        // We count character frequencies from the first string
+        // and subtract frequencies using the second string.
         for (char c : s)
             v[c - 'a']++;
         for (char c : t)
@@ -27,6 +32,8 @@ public:
     }
 
     // used when mixed small , capital, alphanumeric, symobls
+    // TC O(n) : O(n)+O(n)+O(n) for 3 loop total O(n)
+    // SC O(k) : O(k) for k unique char in map
     bool isUniverseAnagram(string s, string t)
     {
         if (s.length() != t.length())
@@ -44,20 +51,36 @@ public:
         }
         for (auto it : str)
         {
-            if(it.second!=0){
+            if (it.second != 0)
+            {
                 return false;
             }
         }
         return true;
     }
 
+    // Whem use sorting
+    // TC O(n log n) : O(n log n)+O(n log n) for 2 time sorting total O(n log n)
+    // SC O(1) : O(26) for constant elements
+    bool isSortAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
+    }
 };
 
 int main()
 {
     IsAnagram a;
     // bool ans = a.isAnagram("abcde", "decba");
-    bool ans = a.isUniverseAnagram("abcde", "decba");
+    // bool ans = a.isUniverseAnagram("abcde", "decba");
+    bool ans = a.isSortAnagram("abcde", "decba");
     cout << boolalpha << ans;
 }
 
